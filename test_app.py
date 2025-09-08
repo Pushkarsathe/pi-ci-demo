3658f89535ccb586ae1b5579e1c1b2573d64cfec
Pushkar <pushkarmsathe@gmail.com>
2025-09-08 11:54:53 +0530

    Initial commit with app, test, deploy, CI

--- test_app.py
+++ test_app.py
@@ -1,4 +1,5 @@
 #test_app.py
+'''
 import unittest
 from app import add
 
@@ -7,6 +8,17 @@ class TestAdd(unittest.TestCase):
         self.assertEqual(add(2, 3), 5)
     def test_add_negative(self):
         self.assertEqual(add(-1, 1), 0)
+
+if __name__ == "__main__":
+    unittest.main()
+'''
+
+import unittest
+from app import add
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
 
 if __name__ == "__main__":
     unittest.main()
